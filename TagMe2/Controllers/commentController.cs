a5cb618fe55465e0d183d73865260116b70e5263
Daryl <43867408+dellod@users.noreply.github.com>
2020-03-11 16:25:17 -0600

    Added commentController, nothing inside yet.

---
+++ TagMe2/Controllers/commentController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TagMe2.Controllers
+{
+    public class commentController : Controller
+    {
+
+    }
+}
