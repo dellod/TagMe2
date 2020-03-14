e638776580833cc78cbb49e0be71f270897163a0
yossriK <yossri.khalil@gmail.com>
2020-03-13 22:09:53 -0600

    one sql query for Addimage. dummy values. GUID need a chagne

--- TagMe2/Controllers/PostController.cs
+++ TagMe2/Controllers/PostController.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using TagMe2.Models;
 
@@ -17,10 +20,23 @@ namespace TagMe2.Controllers
         }
 
         [HttpPost]
-        public String Add(Post Image)
+        public IActionResult Add(Post Image)
         {
+            
+            
+                SqlConnection connection = new SqlConnection("Data Source=tagme.database.windows.net;Initial Catalog=tagme;Persist Security Info=True;User ID=tagme;Password=password401!");
+                connection.Open();
+                string commandText = "Insert into Post (UUID,text, image_url) VALUES(@Id,@tx,@url)";
+                SqlCommand cmd = new SqlCommand(commandText, connection);
+                cmd.Parameters.AddWithValue("@Id", 369);
+                cmd.Parameters.AddWithValue("@tx", "asdfasdf");
+                cmd.Parameters.AddWithValue("@url", "adfadsf");
 
-            return Image.Caption;
+              
+                cmd.ExecuteNonQuery();
+                connection.Close();
+
+            return View();
 
         }
     }
