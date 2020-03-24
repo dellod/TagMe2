c87329057397496acee471641f46208f06f39f88
Will Huang <jenwei.huang@ucgalgary.ca>
2020-03-24 14:18:26 -0600

    Added some query

---
+++ TagMe2/Models/Query/CommandQuery.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using TagMe2.Models.Comments;
+
+namespace TagMe2.Models.Query
+{
+    public static class CommandQuery
+    {
+       
+        public static void AddComment(Comment comment)
+        {
+            
+            string queryString = "CREATE TABLE test(myuser int);";
+            
+            SqlCommand command = new SqlCommand(queryString, connectDatabase.myConnection);
+            command.ExecuteNonQuery();
+        }
+
+        public static void DeleteComment(Comment comment)
+        {
+            string queryString = "CREATE TABLE test(myuser int);";
+        }
+
+        public static void EditComment(Comment comment)
+        {
+            string queryString = "CREATE TABLE test(myuser int);";
+        }
+    }
+}
---
+++ TagMe2/Models/Query/connectDatabase.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TagMe2.Models.Query
+{
+    public static class connectDatabase
+    {
+        public static SqlConnection myConnection
+        {
+            get; set;
+        }
+        
+
+        public static void connectToDatabase()
+        {
+            string connectionString = "Data Source=tagme.database.windows.net;Initial Catalog=tagme;Persist Security Info=True;User ID=tagme;Password=password401!";
+
+            SqlConnection myConnection = new SqlConnection(connectionString);
+         
+                try
+                {
+                    myConnection.Open();
+                    if (myConnection.State == System.Data.ConnectionState.Open)
+                    {
+                        Console.WriteLine("Success");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Not open");
+                    }
+                }
+                catch (SqlException ex)
+                {
+                    Console.WriteLine("Connection failed***************");
+                }
+
+
+        }
+    }
+}
