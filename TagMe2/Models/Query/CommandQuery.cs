60220f2908d16dadb2c80b54be9bcaaa7e660ecd
Will Huang <jenwei.huang@ucgalgary.ca>
2020-03-25 17:29:04 -0600

    Add query to comment event table

--- TagMe2/Models/Query/CommandQuery.cs
+++ TagMe2/Models/Query/CommandQuery.cs
@@ -12,21 +12,33 @@ namespace TagMe2.Models.Query
        
         public static void AddComment(Comment comment)
         {
-            
-            string queryString = "CREATE TABLE test(myuser int);";
-            
+ 
+            string temp = "INSERT INTO CommentEvent VALUES({0},{1},{2},{3},{4},{5});";
+            string queryString = string.Format(temp, comment.ID, comment.Parent_ID, comment.Text, comment.Author, 1);
+
             SqlCommand command = new SqlCommand(queryString, connectDatabase.myConnection);
             command.ExecuteNonQuery();
         }
 
         public static void DeleteComment(Comment comment)
         {
-            string queryString = "CREATE TABLE test(myuser int);";
+            string temp = "INSERT INTO CommentEvent VALUES({0},{1},{2},{3},{4},{5});";
+            string queryString = string.Format(temp, comment.ID, comment.Parent_ID, comment.Text, comment.Author, 2);
+
+            SqlCommand command = new SqlCommand(queryString, connectDatabase.myConnection);
+            command.ExecuteNonQuery();
         }
 
         public static void EditComment(Comment comment)
         {
-            string queryString = "CREATE TABLE test(myuser int);";
+            string temp = "INSERT INTO CommentEvent VALUES({0},{1},{2},{3},{4},{5});";
+            string queryString = string.Format(temp, comment.ID, comment.Parent_ID, comment.Text, comment.Author, 3);
+
+            SqlCommand command = new SqlCommand(queryString, connectDatabase.myConnection);
+            command.ExecuteNonQuery();
         }
     }
+
+        
 }
+    
