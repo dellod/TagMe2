3b8a692532966e7baaaccda6b3535808e410ce30
Daryl Dang <43867408+dellod@users.noreply.github.com>
2020-03-26 11:55:24 -0600

    Minor change

--- TagMe2/Models/Comments/EventSourcingManager.cs
+++ TagMe2/Models/Comments/EventSourcingManager.cs
@@ -5,13 +5,13 @@ using System.Threading.Tasks;
 
 namespace TagMe2.Models.Comments
 {
-    public class EventSourcingManager
+    public static class EventSourcingManager
     {
         /// <summary>
         /// Will retrieve comments under common post.
         /// </summary>
         /// <param name="postID"></param>
-        public void retrieveComments(Guid postID)
+        public static void retrieveCommentsFromPost(Guid postID)
         {
             // TODO need to query based on post ID
             // ORDER will be oldest to newest, so the first comment will be directly attached to the post, and the newest posts will follow afterwards.
