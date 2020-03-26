88978598d20cd2519f69408398b3f341567692d9
Daryl Dang <43867408+dellod@users.noreply.github.com>
2020-03-26 11:52:49 -0600

    Moved over content from query folder into main command handler.

--- TagMe2/Models/Comments/CommentCommandHandler.cs
+++ TagMe2/Models/Comments/CommentCommandHandler.cs
@@ -1,17 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 using TagMe2.Models.Comments;
-
+using TagMe2.Models.Query;
 namespace TagMe2.Models.Comments
 {
     /// <summary>
     /// Handles updates for comments.
     /// </summary>
-    public class CommentCommandHandler
+    public static class CommentCommandHandler
     {
-        void execute(Comment comment)
+        static void execute(Comment comment)
         {
             Commands command = comment.Command;
             switch(command)
@@ -28,19 +29,31 @@ namespace TagMe2.Models.Comments
                     
             }
         }
-        void AddNewComment(Comment comment)
+        static void AddNewComment(Comment comment)
         {
+            string temp = "INSERT INTO CommentEvent VALUES({0},{1},{2},{3},{4},{5});";
+            string queryString = string.Format(temp, comment.ID, comment.Parent_ID, comment.Text, comment.Author, 1);
 
+            SqlCommand command = new SqlCommand(queryString, connectDatabase.myConnection);
+            command.ExecuteNonQuery();
         }
 
-        void DeleteComment(Comment comment)
+        static void DeleteComment(Comment comment)
         {
+            string temp = "INSERT INTO CommentEvent VALUES({0},{1},{2},{3},{4},{5});";
+            string queryString = string.Format(temp, comment.ID, comment.Parent_ID, comment.Text, comment.Author, 2);
 
+            SqlCommand command = new SqlCommand(queryString, connectDatabase.myConnection);
+            command.ExecuteNonQuery();
         }
 
-        void EditComment(Comment comment)
+        static void EditComment(Comment comment)
         {
+            string temp = "INSERT INTO CommentEvent VALUES({0},{1},{2},{3},{4},{5});";
+            string queryString = string.Format(temp, comment.ID, comment.Parent_ID, comment.Text, comment.Author, 3);
 
+            SqlCommand command = new SqlCommand(queryString, connectDatabase.myConnection);
+            command.ExecuteNonQuery();
         }
     }
 }
