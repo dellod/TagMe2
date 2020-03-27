6ee581e6bc676e71b78015ed23dafdc1a1639a56
Will Huang <jenwei.huang@ucgalgary.ca>
2020-03-26 20:24:46 -0600

    add query for serching comment based on guid

--- TagMe2/Models/Comments/CommentQueryHandler.cs
+++ TagMe2/Models/Comments/CommentQueryHandler.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
+using TagMe2.Models.Query;
 
 /// <summary>
 /// Handles read commands for comments.
@@ -13,7 +15,37 @@ namespace TagMe2.Models.Comments
         public static Comment displayComment(Guid id)
         {
             //have query here to look for comment based on ID
-            return null;
+
+            string temp = "SELECT * " +
+                          "FROM CommentEvent" +
+                          "WHERE UUID = {0} ;";
+
+            string queryString = string.Format(temp, id);
+
+            SqlCommand command = new SqlCommand(queryString, connectDatabase.myConnection);
+            SqlDataReader reader = command.ExecuteReader();
+
+            Guid ID;
+            Guid Parent_ID;
+            Guid Post_ID;
+            string Text;
+            Guid User_ID;
+
+
+            reader.Read();
+           
+                ID = Guid.Parse(reader["UUID"].ToString());
+                Parent_ID = Guid.Parse(reader["parent_UUID"].ToString());
+                Post_ID = Guid.Parse(reader["post_ID"].ToString());
+                Text = reader["text"].ToString();
+                User_ID = Guid.Parse(reader["author"].ToString());
+                Comment myComment = new Comment(ID, Parent_ID, Post_ID, Text, new User());
+
+ 
+    
+
+            reader.Close();
+            return myComment;
         }
     }
 }
--- TagMe2/Models/Comments/EventSourcingManager.cs
+++ TagMe2/Models/Comments/EventSourcingManager.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
+using TagMe2.Models.Query;
 
 namespace TagMe2.Models.Comments
 {
@@ -17,6 +20,36 @@ namespace TagMe2.Models.Comments
             // TODO need to query based on post ID, then after query by order
             //          ORDER will be oldest to newest, so the first comment will be directly attached to the post, and the newest posts will follow afterwards.
 
+            string temp = "SELECT * " +
+                          "FROM CommentEvent" +
+                          "WHERE post_ID = {0} ;";
+            string queryString = string.Format(temp,postID);
+
+            SqlCommand command = new SqlCommand(queryString, connectDatabase.myConnection);
+            SqlDataReader reader = command.ExecuteReader();
+
+            Guid ID;
+            Guid Parent_ID;
+            Guid Post_ID;
+            string Text;
+            Guid User_ID;
+ 
+
+
+
+            while (reader.Read())
+            {
+               ID = Guid.Parse(reader["UUID"].ToString());
+               Parent_ID = Guid.Parse(reader["parent_UUID"].ToString());
+               Post_ID = Guid.Parse(reader["post_ID"].ToString());
+               Text = reader["text"].ToString();
+               User_ID = Guid.Parse(reader["author"].ToString());
+               Comment myComment= new Comment(ID, Parent_ID, Post_ID, Text,new User());
+
+               commentList.AddLast(myComment);
+            }
+
+            reader.Close();
             // FIrst query the comment with parent_id that matches the post_id
             // then query the next comment that has parent_id that matches the last comment
             // keep going
